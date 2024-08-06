9d27967c911c956096e2b1d9c6571b85e34d1865
sebhoron <92864754+sebhoron@users.noreply.github.com>
2024-08-06 13:33:46 +0100

    add joi validation

--- app/config/langfuse.js
+++ app/config/langfuse.js
@@ -1,9 +1,22 @@
+const Joi = require('joi')
 const { CallbackHandler } = require('langfuse-langchain')
+
+const schema = Joi.object({
+  publicKey: Joi.string().required(),
+  secretKey: Joi.string().required(),
+  baseUrl: Joi.string().required()
+})
 
 const langfuseParams = {
   publicKey: process.env.LANGFUSE_PUBLIC_KEY,
   secretKey: process.env.LANGFUSE_SECRET_KEY,
   baseUrl: process.env.LANGFUSE_BASEURL
+}
+
+const { error, value } = schema.validate(langfuseParams, { abortEarly: false })
+
+if (error) {
+  throw new Error('DB Config Invalid: ', error.message)
 }
 
 const langfuseHandler = process.env.LANGFUSE_ENABLED ? new CallbackHandler(langfuseParams) : null
