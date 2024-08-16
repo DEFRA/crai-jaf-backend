a41ae5bb63d0507fcce9017f485bcfc898c47014
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2024-08-16 16:25:38 +0100

    Fix bugs (#6)
    * Fix LangFuse config
    
    * Fix bugs
    
    * Remove empty line
    
    * Fix prompts
    
    * Fix LangFuse config
    
    * Add profession storage
    
    ---------
    
    Co-authored-by: sebhoron <92864754+sebhoron@users.noreply.github.com>

--- app/config/langfuse.js
+++ app/config/langfuse.js
@@ -2,25 +2,27 @@ const Joi = require('joi')
 const { CallbackHandler } = require('langfuse-langchain')
 
 const schema = Joi.object({
-  publicKey: Joi.string().required(),
-  secretKey: Joi.string().required(),
-  baseUrl: Joi.string().required()
+  enabled: Joi.boolean().optional(),
+  publicKey: Joi.string().when('enabled', { is: true, then: Joi.required() }),
+  secretKey: Joi.string().when('enabled', { is: true, then: Joi.required() }),
+  baseUrl: Joi.string().when('enabled', { is: true, then: Joi.required() })
 })
 
 const config = {
+  enabled: process.env.LANGFUSE_ENABLED,
   publicKey: process.env.LANGFUSE_PUBLIC_KEY,
   secretKey: process.env.LANGFUSE_SECRET_KEY,
   baseUrl: process.env.LANGFUSE_BASEURL
 }
 
-const { error, value } = schema.validate(config, { abortEarly: false })
+const { err, value } = schema.validate(config, { abortEarly: false })
 
-if (error) {
-  throw new Error('Invalid LangFuse Config: ', error.message)
+if (err) {
+  throw new Error('Invalid LangFuse Config: ', err.message)
 }
 
-const langfuseHandler = process.env.LANGFUSE_ENABLED ? new CallbackHandler(config) : null
+const handler = new CallbackHandler(value)
 
 module.exports = {
-  langfuseHandler
+  langfuse: value.enabled ? [handler] : []
 }
--- app/server.js
+++ app/server.js
@@ -8,6 +8,7 @@ const server = Hapi.server({
 const routes = [].concat(
   require('./routes/healthy'),
   require('./routes/healthz'),
+  require('./routes/jaf/profession'),
   require('./routes/jaf/repository'),
   require('./routes/jaf/compare')
 )
